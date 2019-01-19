6eafa12d57ec806d5695e65b98975a6124c43a41
Bryan Dijkhuizen <dijkhuizenb@outlook.com>
2019-01-19 02:31:11 +0100

    Update Controller.java

--- src/me/project/controller/Controller.java
+++ src/me/project/controller/Controller.java
@@ -2,7 +2,11 @@ package me.project.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 
 import me.project.abstracts.AbstractController;
 import me.project.logic.CarParkingLogic;
@@ -65,6 +69,7 @@ public class Controller extends AbstractController implements ActionListener {
 		oneDay = new JButton("oneDay");
 		oneDay.addActionListener(this);
 		add(oneDay);
+
 		
 	}
 	
